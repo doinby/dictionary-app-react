ff9a79e6496663da97d1ed67b51cbfc220ff6884
KC Do <contact@doinby.co>
2025-06-08 22:41:01 +1000

    add form submission function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import searchIcon from '../assets/images/icon-search.svg';
 
 export default function Search({ setQuery }) {
+	const [input, setInput] = useState('');
+
 	function handleChange(e) {
-		setQuery(e.target.value);
+		// setTimeout(() => setInput(e.target.value), 1000);
+		setInput(e.target.value);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		setQuery(input);
 	}
 
 	return (
-		<form className='relative flex items-center'>
-			<img src={searchIcon} className='absolute right-3 h-3.5' />
+		<form onSubmit={handleSubmit} className='relative flex items-center'>
+			<button type='submit' className='absolute right-3 cursor-pointer'>
+				<img src={searchIcon} className='h-3.5' />
+			</button>
 			<input
 				onChange={handleChange}
+				value={input}
 				className='w-full px-4 py-2 rounded-md bg-slate-200'
 			/>
 		</form>
