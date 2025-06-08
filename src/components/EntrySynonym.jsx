0f9c663fa7c78bbd7ea5fb62277c3f259434baf2
KC Do <contact@doinby.co>
2025-06-08 22:40:48 +1000

    fix overflow list of synonyms

--- src/components/EntrySynonym.jsx
+++ src/components/EntrySynonym.jsx
@@ -1,5 +1,3 @@
-import { getDisplayStyle } from '../ultils/ultils';
-
 export default function EntrySynonym({ synonyms, antonyms }) {
 	if (!synonyms && antonyms?.length) {
 		return (
@@ -12,10 +10,14 @@ export default function EntrySynonym({ synonyms, antonyms }) {
 		);
 	} else if (synonyms?.length) {
 		return (
-			<div className={'flex items-baseline gap-4'}>
+			<div className={'flex flex-wrap items-baseline gap-4'}>
 				<h3 className='text-lg text-slate-500'>Synonyms</h3>
 				{synonyms.map((word) => {
-					return <p key={word}>{word}</p>;
+					return (
+						<p key={word} className='text-lavender'>
+							{word}
+						</p>
+					);
 				})}
 			</div>
 		);
