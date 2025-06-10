bf9b506a653a3fffec755f548c7415a1bf310358
KC Do <contact@doinby.co>
2025-06-11 01:17:25 +1000

    add toggle menu function

--- src/components/FontMenu.jsx
+++ src/components/FontMenu.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import arrowDown from '../assets/images/icon-arrow-down.svg';
 import { ThemeContext } from '../ultils/ultils';
 import { FONTS } from '../ultils/ultils';
@@ -8,16 +8,32 @@ export default function FontMenu() {
 	const btnLabel = FONTS.find((f) => f.ref === font).name;
 	const fontOptions = FONTS.filter((f) => f.ref !== font);
 
+	const [isHidden, setIsHidden] = useState(true);
+	const menuDisplay = isHidden ? 'hidden' : 'block';
+	function toggleMenu() {
+		setIsHidden(!isHidden);
+	}
+
 	return (
 		<div className='relative'>
-			<button className='px-3 py-2 flex gap-2 items-baseline'>
+			<button
+				onClick={toggleMenu}
+				className='px-3 py-2 flex gap-2 items-baseline cursor-pointer'
+			>
 				{btnLabel}
 				<img src={arrowDown} className='w-4' />
 			</button>
-			<ul className='absolute right-0 px-4 py-2 z-1 bg-lavender text-white'>
-				{fontOptions.map(({ name, ref }) => (
+			<ul
+				className={`absolute right-0 z-1 bg-lavender text-white cursor-pointer ${menuDisplay}`}
+			>
+				{fontOptions.map(({ name: label, ref }) => (
 					<li key={ref}>
-						<button onClick={() => setFont(ref)}>{name}</button>
+						<button
+							onClick={() => setFont(ref)}
+							className='min-w-30 px-4 py-2 text-left cursor-pointer'
+						>
+							{label}
+						</button>
 					</li>
 				))}
 			</ul>
