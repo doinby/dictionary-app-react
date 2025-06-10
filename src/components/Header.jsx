7475962c0eb6be6eafbf6f2179ec808d458dd1d1
KC Do <contact@doinby.co>
2025-06-11 00:57:58 +1000

    add FontMenu to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,13 @@
 import logo from '../assets/images/logo.svg';
 import darkMode from '../assets/images/icon-moon.svg';
-import arrowDown from '../assets/images/icon-arrow-down.svg';
+import FontMenu from './FontMenu';
 
 export default function Header() {
 	return (
 		<nav className='flex justify-between items-center'>
 			<img src={logo} />
 			<div className='flex gap-6 items-center'>
-				<button className='flex gap-2 items-baseline'>
-					San Serif
-					<img src={arrowDown} className='w-4' />
-				</button>
+				<FontMenu />
 				<img src={darkMode} className='h-5' />
 			</div>
 		</nav>
