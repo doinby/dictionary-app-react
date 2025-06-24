77e1e7ded6e503633fdbd4e63398af2b41cf7def
KC Do <contact@doinby.co>
2025-06-24 12:04:41 +1000

    refactor FontMenu

--- src/components/FontMenu.jsx
+++ src/components/FontMenu.jsx
@@ -1,42 +1,32 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import arrowDown from '../assets/images/icon-arrow-down.svg';
 import { ThemeContext } from '../ultils/ultils';
 import { FONTS } from '../ultils/ultils';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 
 export default function FontMenu() {
 	const { font, setFont } = useContext(ThemeContext);
-	const btnLabel = FONTS.find((f) => f.ref === font).name;
+	const menuLabel = FONTS.find((f) => f.ref === font).name;
 	const fontOptions = FONTS.filter((f) => f.ref !== font);
 
-	const [isHidden, setIsHidden] = useState(true);
-	const menuDisplay = isHidden ? 'hidden' : 'block';
-	function toggleMenu() {
-		setIsHidden(!isHidden);
-	}
-
 	return (
-		<div className='relative'>
-			<button
-				onClick={toggleMenu}
-				className='px-3 py-2 flex gap-2 items-baseline cursor-pointer'
-			>
-				{btnLabel}
+		<Menu>
+			<MenuButton className='px-3 py-2 w-[132px] flex gap-2 items-baseline justify-end cursor-pointer'>
+				{menuLabel}
 				<img src={arrowDown} className='w-4' />
-			</button>
-			<ul
-				className={`absolute right-0 z-1 bg-lavender text-white cursor-pointer ${menuDisplay}`}
+			</MenuButton>
+			<MenuItems
+				anchor='bottom'
+				className='flex flex-col items-start rounded cursor-pointer bg-white shadow dark:bg-lavender dark:text-white'
 			>
 				{fontOptions.map(({ name: label, ref }) => (
-					<li key={ref}>
-						<button
-							onClick={() => setFont(ref)}
-							className='min-w-30 px-4 py-2 text-left cursor-pointer'
-						>
+					<MenuItem key={ref}>
+						<button onClick={() => setFont(ref)} className='p-3 cursor-pointer'>
 							{label}
 						</button>
-					</li>
+					</MenuItem>
 				))}
-			</ul>
-		</div>
+			</MenuItems>
+		</Menu>
 	);
 }
